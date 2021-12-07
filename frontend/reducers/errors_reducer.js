e3986994a2c09b6587d2152d279c52dcc602f86f
tkoh13 <tylerkoh93@gmail.com>
2021-12-07 17:28:20 -0500

    add user errors

--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import sessionErrorsReducer from './session_errors_reducer';
 import pinErrorsReducer from './pin_errors_reducer';
 import boardsErrorsReducer from './board_reducer';
+import userErrorsReducer from './user_errors_reducer';
 
 const errorsReducer = combineReducers({
     session: sessionErrorsReducer,
     pins: pinErrorsReducer,
     board: boardsErrorsReducer,
+    user: userErrorsReducer
 });
 
 export default errorsReducer;
