c2f43e0b436585f1e84d6350ab901948a3d670cb
tkoh13 <tylerkoh93@gmail.com>
2021-12-07 14:55:04 -0500

    add user-icon

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
 // import SignUpFormContainer from '../session_form/signup_form_container'
 // import LogInFormContainer from '../session_form/login_form_container';
 
