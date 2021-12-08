bcd5de1d7461e3dfa109072756d0c9fac7977049
tkoh13 <tylerkoh93@gmail.com>
2021-12-08 18:00:11 -0500

    add dropdown component

---
+++ frontend/components/dropdown/DropDownMenu.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useRef } from 'react';
+import { closeOnEscape, closeOnOutsideClick } from '../../utils/close_util';
+
+const DropDownMenu = (props) => {
+    const { actions, setShowDropDown, type } = props
+
+    const popupRef = useRef();
+
+    closeOnOutsideClick(popupRef, setShowDropDown);
+    closeOnEscape(setShowDropDown);
+
+    let values = [];
+
+    switch (type) {
+        case 'nav':          
+            Object.values(actions)
+            values = ['Settings', 'About', 'Log out'];
+            // values = Object.keys(actions);
+            break;
+    }
+
+    return (
+        <div className="dropdown-menu" ref={popupRef} >
+            {values.map((val, i) => (
+                <div className="dropdown-item" key={i}>{val}</div>
+            ))}
+        </div>
+    )
+}
+
+export default DropDownMenu
---
+++ frontend/utils/close_util.js
@@ -0,0 +1,40 @@
+import { useEffect } from 'react';
+
+export const closeOnEscape = (setBoolean) => {
+    const checkForEscape = (e) => {
+        if (e.keyCode === 27) {
+            setBoolean(false);
+        }
+    };
+    useEffect(() => {
+        document.addEventListener("keydown", checkForEscape);
+        return () => document.removeEventListener("keydown", checkForEscape);
+    }, []);
+}
+
+export const closeOnOutsideClick = (ref, setBoolean) => {
+    const checkOutside = (e) => {
+        if (ref.current && !ref.current.contains(e.target)) {
+            setBoolean(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener("click", checkOutside);
+        return () => document.removeEventListener("click", checkOutside);
+    }, []);
+}
+
+export const closeModalOnEscape = (closeModal) => {
+    const checkForEscape = (e) => {
+        if (e.keyCode === 27) {
+            e.stopImmediatePropagation();
+            closeModal();
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener("keydown", checkForEscape, true);
+        return () => document.removeEventListener("keydown", checkForEscape, true);
+    }, []);
+}
