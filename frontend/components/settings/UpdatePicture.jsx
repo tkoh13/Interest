84812e12be8d0d039cf9925ffd1688f40518ad71
tkoh13 <tylerkoh93@gmail.com>
2021-12-10 12:01:11 -0500

    UpdatePicture: add handleFile and handleSubmit

--- frontend/components/settings/UpdatePicture.jsx
+++ frontend/components/settings/UpdatePicture.jsx
@@ -5,15 +5,44 @@ import React, { Component } from 'react';
 class UpdatePicture extends Component {
     constructor(props) {
         super(props)
+        this.state = {
+            photoFile: null,
+            photoUrl: null,
+            errors: null
+        }
     }
 
+    handleFile(e) {
+        const file = e.currentTarget.files[0];
+        const fileReader = new FileReader();
+        fileReader.onloadend = () => {
+
+            this.setState({ photoFile: file, photoUrl: fileReader.result });
+        };
+        if (file) {
+            fileReader.readAsDataURL(file);
+        }
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        if (!this.state.photoFile) {
+            return this.setState({ errors: 'No picture' })
+        } else {
+            const formData = new FormData();
+            const { photoFile } = this.state;
+            formData.append('user[photo]', photoFile)
+
+            this.props.updateUser(formData)
+            this.props.closeModal()
+        }
+    }
     
     render() {
         return (
             <div className="change-photo-container">
-
                 <h1 className="cpc-h1">Change your picture</h1>
-                <label id="change-photo-button">
+                <label id="change-photo-button" onSubmit={this.handleSubmit}>
                     <span>Choose Photo</span>
                     <input 
                         type="file" 
@@ -21,9 +50,6 @@ class UpdatePicture extends Component {
                         style={{display:"none"}}
                         onChange={this.handleFile} />
                 </label>
-
-                {/* need to make button fatter */}
-
             </div>
         );
     }   
@@ -31,15 +57,15 @@ class UpdatePicture extends Component {
 
 import { connect } from 'react-redux';
 import { updateUser } from '../../actions/user_actions';
+import { closeModal } from '../../actions/modal_actions';
 
 const mapStateToProps = (state) => ({
-    state
-
-
+    currentUser: state.entities.users[state.session.id],
 })
 
 const mapDispatchToProps = dispatch => ({
-    updateUser: (userId, user) => dispatch(updateUser(userId, user))
+    updateUser: (userId, user) => dispatch(updateUser(userId, user)),
+    closeModal: () => dispatch(closeModal())
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(UpdatePicture);
