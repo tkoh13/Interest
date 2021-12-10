053b19ff6676d864d05ef4e51d21c95e37d22efd
tkoh13 <tylerkoh93@gmail.com>
2021-12-10 11:19:07 -0500

    update users_controller user_params for photo

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -28,7 +28,7 @@ class Api::UsersController < ApplicationController
 
     private
     def user_params
-        params.require(:user).permit(:email, :password, :username)
+        params.require(:user).permit(:email, :password, :username, :photo)
     end
 
 end
