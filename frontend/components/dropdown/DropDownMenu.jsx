51bb9815d32c69dca9912a5eea240db6a7864e48
tkoh13 <tylerkoh93@gmail.com>
2021-12-09 22:29:19 -0500

    update DropDownMenu for NavBar

--- frontend/components/dropdown/DropDownMenu.jsx
+++ frontend/components/dropdown/DropDownMenu.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { useRef } from 'react';
+import { Link } from 'react-router-dom';
 import { closeOnEscape, closeOnOutsideClick } from '../../utils/close_util';
 
 const DropDownMenu = (props) => {
     const { actions, setShowDropDown, type } = props
-
+    const { currentUser, logout } = actions
+    // actions.logout(), actions.openModal(modal), actions.currentUser
     const popupRef = useRef();
 
     closeOnOutsideClick(popupRef, setShowDropDown);
@@ -15,18 +17,27 @@ const DropDownMenu = (props) => {
 
     switch (type) {
         case 'nav':          
-            Object.values(actions)
+            console.log(Object.values(actions)) 
             values = ['Settings', 'About', 'Log out'];
+            component =
+            <div className="dropdown-menu" ref={popupRef} >
+                <Link className="dropdown-item" to={`/settings`}>Settings</Link>
+                <div className="dropdown-item" >About</div>
+                <div className="dropdown-item" onClick={logout}>Log out</div>
+            </div>
             // values = Object.keys(actions);
             break;
     }
 
     return (
-        <div className="dropdown-menu" ref={popupRef} >
-            {values.map((val, i) => (
-                <div className="dropdown-item" key={i}>{val}</div>
-            ))}
+        <div>
+            {component}
         </div>
+        // <div className="dropdown-menu" ref={popupRef} >
+        //     {values.map((val, i) => (
+        //         <div className="dropdown-item" key={i}>{val}</div>
+        //     ))}
+        // </div>
     )
 }
 
