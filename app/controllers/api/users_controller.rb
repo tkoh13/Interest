03f4c4f10b0dc2557f0a189583bf0d23f9c1279d
tkoh13 <tylerkoh93@gmail.com>
2021-12-12 19:12:46 -0500

    fix users_controller typo

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -10,17 +10,15 @@ class Api::UsersController < ApplicationController
     end
 
     def show
-        # @user = User.find(params[:userId])
-        @user = User.find_by(id: params[:id])
+        @user = User.find(params[:id])
         render "api/users/show"
         # need to factor in showing users from follows
         # @user = User.where(id: params[:id]).includes(:followers, :followees)first
     end
 
     def update
-        @user = User.find_by(id: params[:id])
-
-        if @user = current_user
+        @user = User.find(params[:id])
+        if @user == current_user
             if @user.update(user_params)
                 render 'api/users/show'
             else
