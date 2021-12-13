3dce898da1b3e0a057f9d13f2a9132b9793e8e9d
tkoh13 <tylerkoh93@gmail.com>
2021-12-12 19:09:38 -0500

    edit migration for add private column to board

--- db/migrate/20211212183159_add_private_to_boards.rb
+++ db/migrate/20211212183159_add_private_to_boards.rb
@@ -1,5 +1,5 @@
 class AddPrivateToBoards < ActiveRecord::Migration[5.2]
   def change
-    add_column :boards, :private, :boolean
+    add_column :boards, :private, :boolean, :default => false
   end
 end
