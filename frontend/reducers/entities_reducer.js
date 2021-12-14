f8179892f8ff58c5f345ae8c6d031246bac13ba4
tkoh13 <tylerkoh93@gmail.com>
2021-12-14 15:33:46 -0500

    Add follow reducers to root reducers

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux'
 import usersReducer from './users_reducer'
 import pinsReducer from './pins_reducer';
-import boardsReducer from './board_reducer';
+import boardsReducer from './boards_reducer';
+import followsReducer from './follows_reducer';
 
 const entitiesReducer = combineReducers({
   users: usersReducer,
   pins: pinsReducer,
-  boards: boardsReducer
+  boards: boardsReducer,
+  follows: followsReducer
 });
 
 export default entitiesReducer;
