fe970134500ba1b514906dcc717ebc41e5df2a7e
tkoh13 <tylerkoh93@gmail.com>
2021-12-14 14:37:34 -0500

    Update routes and follow model

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,2 +1,12 @@
 class Follow < ApplicationRecord
+    validates :follower_id, :followed_id, presence: true
+    validates :follower_id, uniqueness: {scope: :followed_id}
+
+    belongs_to :follower,
+      class_name: :User,
+      foreign_key: :follower_id
+
+    belongs_to :followed, 
+      class_name: :User,
+      foreign_key: :followed_id
 end
