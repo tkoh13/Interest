e13b988388f4f0a226ec577417ceadfbd710d2ad
tkoh13 <tylerkoh93@gmail.com>
2021-12-14 12:02:36 -0500

    PinDetail add 'zoom out' to home

--- frontend/components/pins/PinDetails.jsx
+++ frontend/components/pins/PinDetails.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class PinDetails extends Component{
     constructor(props) {
@@ -34,6 +35,7 @@ class PinDetails extends Component{
             const { user } = this.state;
 
         return(
+            <Link to='/'>
             <div className='pin-details-overlay'>
                 <div className='pin-details-container'>
                     <div className='pin-details-left'>
@@ -56,6 +58,7 @@ class PinDetails extends Component{
                     </div>
                 </div>
             </div>
+            </Link>
         )
         }
     }
