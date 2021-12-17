8211fd4837a5429e461b4bfd4cff8bf0109ee05e
tkoh13 <tylerkoh93@gmail.com>
2021-12-17 16:39:10 -0500

    Update boardtopins _controller

--- app/controllers/api/boardtopins_controller.rb
+++ app/controllers/api/boardtopins_controller.rb
@@ -2,18 +2,9 @@ class BoardToPins < ApplicationController
     def create
         @board_to_pin = BoardToPin.new(board_to_pin_params)
         if @board_to_pin.save
-            render 'api/boardtopins/show'
+            render json: ["Saved pin to board"], status: 200
         else
             render json: @board_to_pin.errors.full_messages, status: 422
-        end
-    end
-
-    def show
-        @board_to_pin = BoardToPin.find_by(id: params[:id])
-        if !@board_to_pin.nil?
-            render 'api/boardtopins/show'
-        else
-            render json: ["invalid request"], status: 400
         end
     end
 
@@ -28,12 +19,12 @@ class BoardToPins < ApplicationController
     end
     
     def destroy
-        @board_to_pin = BoardToPin.find_by(id: params[:id])
-        if @board_to_pin && ( @board_to_pin.board_id.creator_id == current_user.id )
+        @board_to_pin = BoardToPin.find(params[:id])
+        if @board_to_pin && ( @board_to_pin.user.id == current_user.id )
             @board_to_pin.destroy
-
+            render json: ["Deleted pin from board"], status: 200 
         else
-            render json: ['invalid request'], status: 404
+            render json: ["Invalid request"], status: 404
         end
     end
 
