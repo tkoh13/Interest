3e7125ce9c7cc8a25e5ab0859744f839c4737e16
tkoh13 <tylerkoh93@gmail.com>
2021-12-17 16:52:33 -0500

    Update root reducers with save reducers

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -3,12 +3,14 @@ import usersReducer from './users_reducer'
 import pinsReducer from './pins_reducer';
 import boardsReducer from './boards_reducer';
 import followsReducer from './follows_reducer';
+import savesReducer from './saves_reducer';
 
 const entitiesReducer = combineReducers({
   users: usersReducer,
   pins: pinsReducer,
   boards: boardsReducer,
-  follows: followsReducer
+  follows: followsReducer,
+  saves: savesReducer
 });
 
 export default entitiesReducer;
