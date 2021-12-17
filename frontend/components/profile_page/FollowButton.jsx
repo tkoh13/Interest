63b2607f22969717b0c3595b9c7fd859eb9a29b9
tkoh13 <tylerkoh93@gmail.com>
2021-12-17 11:17:09 -0500

    Remove comments FollowButton

--- frontend/components/profile_page/FollowButton.jsx
+++ frontend/components/profile_page/FollowButton.jsx
@@ -11,26 +11,15 @@ class FollowButton extends Component {
     }
 
     componentDidMount() {
-        // console.log("mounted")
         const { user, currentUser } = this.props;
         const { following } = currentUser
-        // debugger
         if (following.filter(u => u.id === user.id)[0]) {
             this.setState({ status: true })
         }
     }
 
-    // componentDidUpdate(prevProps, prevState) {
-    //     console.log(prevProps)
-    //     console.log(prevState)
-    //     if (prevState.status !== this.state.status) this.renderButton()
-    // }
-
     handleFollow() {
-        console.log('handleFollow')
-        // this.setState({ status: true })
         const { createFollow, currentUser, user } = this.props;
-        // debugger
         const follow = {
             follower_id: currentUser.id,
             followee_id: user.id,
@@ -40,12 +29,9 @@ class FollowButton extends Component {
     }
 
     handleUnFollow() {
-        console.log('handleUnFollow')
         this.setState({ status: false })
-        debugger
         const { deleteFollow, follows, user, currentUser } = this.props
-        const followId = follows.filter(e => e.followee_id === user.id && e.follower_id === currentUser.id)[0]["id"]
-        // const { deleteFollow } = this.props;
+        const followId = follows.filter(e => e.followee_id === user.id && e.follower_id === currentUser.id)[0]["id"]        
         deleteFollow(followId)
             .then(() => this.setState({ status: false }));
     }
