20ab4a6f99a25b20c7b87fddc16cbbd7e1d01d26
tkoh13 <tylerkoh93@gmail.com>
2021-12-21 15:00:15 -0500

    Complete BoardShow

--- frontend/components/boards/BoardShow.jsx
+++ frontend/components/boards/BoardShow.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import PinDisplay from '../pins/PinDisplay'
 import { MdOutlineModeEditOutline } from 'react-icons/md'
 import { CgLock } from 'react-icons/cg'
 
@@ -8,7 +9,8 @@ class BoardShow extends Component {
         super(props);
         this.state = {
             board: this.props.board,
-            user: null
+            user: null,
+            pinContent: null
         }
     }
 
@@ -16,36 +18,70 @@ class BoardShow extends Component {
         const { fetchBoard, match: { params} } = this.props
         fetchBoard(params.boardId)
             .then(() => this.setState({ board: this.props.board }))
-        // console.log(this.state)
     }
 
     componentDidUpdate(prevProps) {
         const { fetchUser, board, users } = this.props
         if (prevProps.board !== board) {
-            // debugger
+            this.buildPinsDisplay();
             fetchUser(board.creator_id)
-                .then(() => this.setState({ user: users[board.creator_id] }))
-                .then(() => console.log(this.state))
-                console.log(this.state)
+                .then(() => this.setState({ user: users[board.creator_id]}))
         }
+    }
+
+    buildPinsDisplay() {
+        const shuffle = (array) => {
+            let currentIndex = array.length, randomIndex;
+            while (currentIndex != 0) {
+                randomIndex = Math.floor(Math.random() * currentIndex);
+                currentIndex--;
+                [array[currentIndex], array[randomIndex]] = [
+                    array[randomIndex], array[currentIndex]];
+            }
+            return array;
+        }
+        const { board } = this.props;
+        if (!board.pins.length) return null;
+        const size = ["small", "medium", "large"]
+        const pinsToDisplay = shuffle(board.pins).map((content) => {
+            return (
+                <PinDisplay content={content} key={content.id} size={size[Math.floor(Math.random() * size.length)]} />
+            )
+        })
+        this.setState({
+            pinContent: pinsToDisplay
+        })
+    }
+
+    renderPins() {
+        if (!this.state.pinContent) return null
+        return (
+            <div className="pin-display-container">
+                {this.state.pinContent}
+            </div>
+        )
     }
 
     render() {
         const { board, user } = this.state;
-
+        const { openModal } = this.props;
         if (!board || !user ) return null
         return(
-            <div>
-                <header>
-                    <h1>{board.title} <MdOutlineModeEditOutline /></h1>
-                    <div className="profile-pic-container">
+            <div className='profile-container'>
+                <header className='ph-main'>
+                    <h1 className='profile-username'>{board.title} 
+                        <span className='bs-edit-icon-container' onClick={() => openModal('editBoard')}>
+                            <MdOutlineModeEditOutline className='bs-edit-icon' size={20} />
+                        </span>
+                            </h1>
+                    <div className="bs-profile-pic-container">
                         {user.photoUrl ? <img src={user.photoUrl} /> : <img src={window.userIcon} />}
                     </div>
-                    {board.private ? <div></div> : <div><CgLock /> Secret board</div>}
+                    {board.private ? <div className='bs-secret'><CgLock /> Secret board</div> : <div></div>}
                 </header>
                 <section>
-                    <div>{board.pins.length === 1 ? `1 Pin` : `${board.pins.length} Pins`}</div>
-                    <div>{board.pins.length === 0 ? "There aren't any Pins on this board yet" : `${board.pins.length} Pins`}</div>
+                    <div className='bs-count'>{board.pins.length === 1 ? `1 Pin` : `${board.pins.length} Pins`}</div>
+                    {board.pins.length === 0 ? <div className='bs-none'>There aren't any Pins on this board yet</div> : <div>{this.renderPins()}</div>}
                 </section>
             </div>
         )
