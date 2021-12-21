cc2838c71921015db6b0535a1389e7fcdcfec648
tkoh13 <tylerkoh93@gmail.com>
2021-12-21 13:44:28 -0500

    Add header information to BoardShow

--- frontend/components/boards/BoardShow.jsx
+++ frontend/components/boards/BoardShow.jsx
@@ -1,21 +1,52 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import { MdOutlineModeEditOutline } from 'react-icons/md'
+import { CgLock } from 'react-icons/cg'
 
 class BoardShow extends Component {
     constructor(props) {
-        super(props)
+        super(props);
+        this.state = {
+            board: this.props.board,
+            user: null
+        }
     }
 
-    // componentDidMount() {
+    componentDidMount() {
+        const { fetchBoard, match: { params} } = this.props
+        fetchBoard(params.boardId)
+            .then(() => this.setState({ board: this.props.board }))
+        // console.log(this.state)
+    }
 
-    // }
+    componentDidUpdate(prevProps) {
+        const { fetchUser, board, users } = this.props
+        if (prevProps.board !== board) {
+            // debugger
+            fetchUser(board.creator_id)
+                .then(() => this.setState({ user: users[board.creator_id] }))
+                .then(() => console.log(this.state))
+                console.log(this.state)
+        }
+    }
 
     render() {
-        const { board } = this.props;
-        if (!board) return null
+        const { board, user } = this.state;
+
+        if (!board || !user ) return null
         return(
             <div>
-                <h1>{board.title}</h1>
+                <header>
+                    <h1>{board.title} <MdOutlineModeEditOutline /></h1>
+                    <div className="profile-pic-container">
+                        {user.photoUrl ? <img src={user.photoUrl} /> : <img src={window.userIcon} />}
+                    </div>
+                    {board.private ? <div></div> : <div><CgLock /> Secret board</div>}
+                </header>
+                <section>
+                    <div>{board.pins.length === 1 ? `1 Pin` : `${board.pins.length} Pins`}</div>
+                    <div>{board.pins.length === 0 ? "There aren't any Pins on this board yet" : `${board.pins.length} Pins`}</div>
+                </section>
             </div>
         )
     }
@@ -24,18 +55,23 @@ class BoardShow extends Component {
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 import { fetchBoard } from '../../actions/board_actions'
+import { fetchUser } from '../../actions/user_actions'
 import { openModal } from '../../actions/modal_actions'
 
 
-const mapStateToProps = ({ session, entities: { boards }}, { match }) => {
+const mapStateToProps = ({ session, entities: { boards, users }}, { match }) => {
     return ({
         board: boards[match.params.boardId],
-        userId: session.id
+        currentUser: users[session.id],
+        users,
+        // user: users[boards[match.params.boardId].creator_id],
+        // userId: session.id
     })
 };
 
 const mapDispatchToProps = dispatch => ({
     fetchBoard: boardId => dispatch(fetchBoard(boardId)),
+    fetchUser: userId => dispatch(fetchUser(userId)),
     openModal: modal => dispatch(openModal(modal)),
 });
 
