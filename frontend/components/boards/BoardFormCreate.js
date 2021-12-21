ae1b68ef4ceca277d5a1465d4722cf0ea8c07c49
tkoh13 <tylerkoh93@gmail.com>
2021-12-21 11:48:15 -0500

    Add BoardFromCreate Container

---
+++ frontend/components/boards/BoardFormCreate.js
@@ -0,0 +1,23 @@
+import { connect } from 'react-redux';
+import { createBoard, deleteBoard } from '../../actions/board_actions';
+import { closeModal } from '../../actions/modal_actions';
+import BoardForm from './BoardForm';
+
+const mapStateToProps = ({ session, errors, entities: { users, boards}}) => ({
+    board: {
+        title: "",
+        description: "",
+        creator_id: session.id
+    },
+    currentUser: users[session.id],
+    error: errors.boards,
+    formType: 'Create'
+});
+
+const mapDispatchToProps = dispatch => ({
+    createBoard: board => dispatch(createBoard(board)),
+    deleteBoard: boardId => dispatch(deleteBoard(boardId)),
+    closeModal: () => dispatch(closeModal()),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(BoardForm);
